86bd8748ace5efda0cb3adaacae684a0a07461c9
Ayushi760 <ayushisaxena760@gmail.com>
2024-01-29 23:52:54 +0530

    First Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,11 @@ import { Tilt } from 'react-tilt'
 const Hero = () => {
   return (
     <section className='relative w-[80%] h-screen mx-auto'>
-      <div className="absolute top-[180px] mx-auto flex flex-col items-center w-full gap-8 xs:gap-6">
+      <div className="absolute top-[200px] mx-auto flex flex-col items-center w-full gap-8 xs:gap-6">
         <h1 className="text-white text-4xl font-bold text-center leading-12 xs:text-2xl" >
           Full Stack Developer, Coder and Artist.
         </h1>
-        <p className='text-white text-xl text-center xs:text-lg'>Dedicated to Continuous Learning and Growth.</p>
+        <p className='text-white text-xl text-center mb-10 xs:text-lg'>Dedicated to Continuous Learning and Growth.</p>
         <Tilt
           options={{
             max: 45,
